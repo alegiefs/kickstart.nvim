7cb912f65dd400fabb2ec6b353fd3a7b41f234d0
Aleksander Giera <aleksander.giera@tieto.com>
2026-05-20 10:14:00 +0200

    adding markdown preview plugin

--- init.lua
+++ init.lua
@@ -892,6 +892,7 @@ do
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
   require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.markdown_preview'
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --
---
+++ lua/kickstart/plugins/markdown_preview.lua
@@ -0,0 +1,12 @@
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+require('render-markdown').setup({
+    html = {
+        comment = {
+            conceal = false, -- Do Not hide comments
+        }
+    }
+})
